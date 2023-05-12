533eb66f8f046aec78419a990e3ef6baebc358c5
Wali-Muhammad123 <renedescartian@gmail.com>
2023-05-13 02:45:30 +0500

    Made changes

--- digigold/settings.py
+++ digigold/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True    
@@ -120,9 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR / "static",
-]
 STATIC_ROOT='/gcptest/static'
 
 # Default primary key field type
